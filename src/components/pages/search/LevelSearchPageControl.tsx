e0b3e525ecd1aefb9f5c72cebe12e3b61f9e337b
UncaughtCursor <UncaughtCursor@gmail.com>
2022-07-05 06:56:08 -0400

    Fix missing type parameter for search settings

--- src/components/pages/search/LevelSearchPageControl.tsx
+++ src/components/pages/search/LevelSearchPageControl.tsx
@@ -1,8 +1,9 @@
 import { LevelSearchResults, numResultsPerPage } from '@scripts/browser/MeilisearchUtil';
+import { LevelSearchFilterSettings } from '@scripts/browser/SearchUtil';
 import { useRouter } from 'next/router';
 import React from 'react';
 import TriggerButton from '../controls/TriggerButton';
-import { getSearchUrl, LevelSearchFilterSettings } from './LevelSearchBar';
+import { getSearchUrl } from './LevelSearchBar';
 
 /**
  * Displays pagination controls for a search result page.
@@ -71,6 +72,7 @@ function LevelSearchPageControl(props: {
 			const query = props.curSearchResults.searchParams.q;
 			const searchParams = props.curSearchResults.searchParams;
 			const newSearchSettings: LevelSearchFilterSettings = {
+				type: searchParams.type,
 				sortType: searchParams.sortType,
 				sortOrder: searchParams.sortOrder,
 				difficulty: searchParams.difficulty,
